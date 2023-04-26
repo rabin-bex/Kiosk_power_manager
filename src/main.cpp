7b85d26d3ebe0d84d3937b5539b4e374b776a0f0
rabin-bex <rabin.bex@gmail.com>
2023-04-26 06:06:43 +0545

    two epprom syncronization 90% complete

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,16 @@ struct
 }uart,uart2;
 
 
+struct schedule
+{
+  uint8_t Start_hour;
+  uint8_t Start_minute;
+  uint8_t End_hour;
+  uint8_t End_minute;
+};
+
+
+
 bool signupOK=false;
 
 FirebaseData fbdo,fbdo_stream;
@@ -57,8 +67,8 @@ class P_Memory
   int get_device_id();
   void set_device_id(int id);
   void clear_all_memory(void);
-  void storeSchedule(uint8_t month, uint8_t day,uint8_t Start_hour, uint8_t Start_minute, uint8_t End_hour, uint8_t End_minute);
-  void getSchedule(uint8_t month, uint8_t day);
+  void storeSchedule(uint8_t month, uint8_t day,schedule sc);
+  schedule getSchedule(uint8_t month, uint8_t day);
 }ESP32_Flash;
 
 static void setup_network(void);
@@ -67,11 +77,10 @@ static void check_firebase_request(void);
 static void check_slave_request(void);
 static bool check_serial();
 static bool check_serial2();
-
-//result.stringValue
-                //String topic=(Firebase.RTDB.getString(&fbdo,"Server_Request/topic")?fbdo.stringData():"";  
-                //String value=Firebase.RTDB.getString(&fbdo,"Server_Request/value")?fbdo.stringData():"";  
-                //Firebase.RTDB.setString(&fbdo,"Device_Response","ok");
+static bool check_month(String val,int * index);
+static bool check_day(String val,uint8_t* month, uint8_t* day);
+static bool check_query(String val,uint8_t* month, uint8_t* day);
+static bool parse_schedule(String value,uint8_t month, uint8_t day,schedule* sc);
 
 void setup() 
 {
@@ -83,8 +92,8 @@ void setup()
   // char buf[50];
   // for(int i=1; i<32;i++)
   // {
-  // sprintf(buf,"Schedule/Dec/%d",i);
-  // Firebase.RTDB.setString(&fbdo,buf,"24:0,24:0,24:0,24:0");
+  // sprintf(buf,"Schedule/Jan/%d",i);
+  // Firebase.RTDB.setString(&fbdo,buf,"0:0,0:0,24:0,24:0");
   // delay(300);
   // }
 }
@@ -93,13 +102,17 @@ void loop()
 {
   check_firebase_request();
   check_serial_request();
-  //check_slave_request();
+  check_slave_request();
 }
 
 static void check_slave_request(void)
 {
-
+   if(check_serial2)
+   {
+    ;
+   }
 }
+
                 
 static void check_firebase_request(void)
 {
@@ -143,23 +156,194 @@ static void check_firebase_request(void)
         }
         else if(topic=="schedule")
         {
-          if (value == "?") goto label;
-          else if(value=="Jan")
+          char buf[50];
+          String val;
+          schedule sc;
+          int index=0;
+          uint8_t m,d;
+          ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          if(value=="Year")
           {
-            // char buf[50];
-            // String val;
-            // for(int i=1; i<32;i++)
-            // {
-            //   sprintf(buf,"Schedule/Jan/%d",i);
-            //   if(Firebase.RTDB.getString(&fbdo,buf))
-            //   {
-            //     val=fbdo.stringData();
-            //     Serial.println(val);
-            //   }
-            //   delay(30);
-            // }
-
+            char month[25];
+            for( int i=0; i<12; i++)
+            {
+              switch(i)
+              {
+                case 0:
+                strcpy(month,"Schedule/Jan/");
+                break;
+                case 1:
+                strcpy(month,"Schedule/Feb/");
+                break;
+                case 2:
+                strcpy(month,"Schedule/Mar/");
+                break;
+                case 3:
+                strcpy(month,"Schedule/Apr/");
+                break;
+                case 4:
+                strcpy(month,"Schedule/May/");
+                break;
+                case 5:
+                strcpy(month,"Schedule/Jun/");
+                break;
+                case 6:
+                strcpy(month,"Schedule/Jul/");
+                break;
+                case 7:
+                strcpy(month,"Schedule/Aug/");
+                break;
+                case 8:
+                strcpy(month,"Schedule/Sep/");
+                break;
+                case 9:
+                strcpy(month,"Schedule/Oct/");
+                break;
+                case 10:
+                strcpy(month,"Schedule/Nov/");
+                break;
+                case 11:
+                strcpy(month,"Schedule/Dec/");
+                break;
+              }
+              Serial.printf("Month:%s\n",month);
+              for(int j=1; j<Number_Of_Days_In_Months[i]+1;j++)
+              {
+                sprintf(buf,"%s%d",month,j);
+                if(Firebase.RTDB.getString(&fbdo,buf))
+                {
+                  val=fbdo.stringData();
+                  if(parse_schedule(val,i+1,j,&sc))
+                  {
+                    sprintf(buf,"{\"topic\":\"schedule\",\"value\":\"%d/%d,%d:%d,%d:%d\"}",i+1,j,sc.Start_hour,sc.Start_minute,sc.End_hour,sc.End_minute);
+                    Serial2.printf("%s",buf);
+                    delay(100);
+                    if(check_serial2())
+                    Serial.printf("%s\n",uart2.buffer);
+                  }
+                }
+                delay(200);
+              }
+           }
+           
+           sprintf(serial,"{\"topic\":\"status\",\"value\":\"ok\"}");
+           if(json.setJsonData(serial))Firebase.RTDB.setJSON(&fbdo,"Device_Response",&json);
           }
+          ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         else if(check_day(value,&m,&d))
+         {
+           char month[25];
+           switch(m)
+           {
+            case 1:
+            strcpy(month,"Schedule/Jan/");
+            break;
+            case 2:
+            strcpy(month,"Schedule/Feb/");
+            break;
+            case 3:
+            strcpy(month,"Schedule/Mar/");
+            break;
+            case 4:
+            strcpy(month,"Schedule/Apr/");
+            break;
+            case 5:
+            strcpy(month,"Schedule/May/");
+            break;
+            case 6:
+            strcpy(month,"Schedule/Jun/");
+            break;
+            case 7:
+            strcpy(month,"Schedule/Jul/");
+            break;
+            case 8:
+            strcpy(month,"Schedule/Aug/");
+            break;
+            case 9:
+            strcpy(month,"Schedule/Sep/");
+            break;
+            case 10:
+            strcpy(month,"Schedule/Oct/");
+            break;
+            case 11:
+            strcpy(month,"Schedule/Nov/");
+            break;
+            case 12:
+            strcpy(month,"Schedule/Dec/");
+            break;
+           }
+           sprintf(buf,"%s%d",month,d);
+           if(Firebase.RTDB.getString(&fbdo,buf))
+           {
+            val=fbdo.stringData();
+            if(parse_schedule(val,m,d,&sc))
+            {
+               sprintf(buf,"{\"topic\":\"schedule\",\"value\":\"%d/%d,%d:%d,%d:%d\"}",m,d,sc.Start_hour,sc.Start_minute,sc.End_hour,sc.End_minute);
+               Serial2.printf("%s",buf);
+               delay(100);
+               if(check_serial2())
+               if(json.setJsonData(uart2.buffer))Firebase.RTDB.setJSON(&fbdo,"Device_Response",&json);
+            }
+          }
+           
+         }
+          ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          else if(check_month(value,&index))
+          {
+            int success_count=0;
+            for(int i=1; i<Number_Of_Days_In_Months[index]+1;i++)
+            {
+              sprintf(buf,"Schedule/%s/%d",value,i);
+              if(Firebase.RTDB.getString(&fbdo,buf))
+              {
+                val=fbdo.stringData();
+                if(parse_schedule(val,index+1,i,&sc))
+                {
+                  success_count++;
+                  sprintf(buf,"{\"topic\":\"schedule\",\"value\":\"%d/%d,%d:%d,%d:%d\"}",index+1,i,sc.Start_hour,sc.Start_minute,sc.End_hour,sc.End_minute);
+                  Serial2.printf("%s",buf);
+                  delay(100);
+                  if(check_serial2())
+                  Serial.printf("%s\n",uart2.buffer);
+                }
+              }
+              delay(200);
+            }
+            if(success_count==Number_Of_Days_In_Months[index])
+            {
+              sprintf(serial,"{\"topic\":\"status\",\"value\":\"ok\"}");
+              if(json.setJsonData(serial))Firebase.RTDB.setJSON(&fbdo,"Device_Response",&json);
+            }
+          }
+         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         else if(check_query(value,&m,&d)) 
+          {
+            Serial2.printf("{\"topic\":\"schedule\",\"value\":\"%d/%d,?\"}",m,d);
+            delay(200);
+            if(check_serial2())
+            {
+              DeserializationError error = deserializeJson(Json.doc, uart2.buffer);
+              if (error) 
+              {
+                Serial.println("{\"topic\":\"status\",\"value\":\"error\"}");
+              }
+              else
+              {
+                 String topic=Json.doc["topic"];
+                 String value=Json.doc["value"];
+
+                 if(topic=="schedule")
+                 {
+                  schedule sc=ESP32_Flash.getSchedule(m,d);
+                  sprintf(serial,"{\"topic\":\"schedule\",\"value\":\"%s,%d:%d,%d:%d\"}",value,sc.Start_hour,sc.Start_minute,sc.End_hour,sc.End_minute);
+                  if(json.setJsonData(serial))Firebase.RTDB.setJSON(&fbdo,"Device_Response",&json);
+                 }
+              }
+
+            }
+          }
+          //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
         }
         else if(topic=="api_key")
         {
@@ -258,6 +442,141 @@ static void check_firebase_request(void)
       }
     }
   }
+}
+
+static bool check_query(String value,uint8_t* month, uint8_t* day)
+{
+  uint8_t start=0;
+  uint8_t end=0;
+  int len=value.length();
+  String sub_val;
+  end=value.indexOf(',',start);
+  if(end<len)
+  {
+    sub_val=value.substring(start,end);
+    if(sub_val!="?")
+    return false;
+  }
+  start=end+1;
+  end=value.indexOf('/',start);
+  if(end<len)
+  {
+    *month=value.substring(start,end).toInt();
+    if(*month<1 && *month>12)
+    return false;
+  }
+  start=end+1;
+  *day=value.substring(start,len).toInt();
+  if(*day>Number_Of_Days_In_Months[*month-1] && *day<1 )return false;
+  return true;
+}
+
+
+
+static bool check_day(String value,uint8_t *month, uint8_t *day)
+{
+  uint8_t start=0;
+  uint8_t end=0;
+  int len=value.length();
+  String sub_val;
+  end=value.indexOf(',',start);
+  if(end<len)
+  {
+    sub_val=value.substring(start,end);
+    if(sub_val!="Day")
+    return false;
+  }
+  start=end+1;
+  end=value.indexOf('/',start);
+  if(end<len)
+  {
+    *month=value.substring(start,end).toInt();
+    if(*month<1 && *month>12)
+    return false;
+  }
+  start=end+1;
+  *day=value.substring(start,len).toInt();
+  if(*day>Number_Of_Days_In_Months[*month-1] && *day<1 )return false;
+  return true;
+}
+
+static bool parse_schedule(String value,uint8_t month, uint8_t day,schedule* sc)
+{
+  uint8_t start=0;
+  uint8_t end=0;
+  schedule sc2;
+  int len=value.length();
+  end=value.indexOf(':',start);
+  if(end<len)
+  {
+    sc->Start_hour=value.substring(start,end).toInt();
+    if(sc->Start_hour>23)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(',',start);
+  if(end<len)
+  {
+    sc->Start_minute=value.substring(start,end).toInt();
+    if(sc->Start_minute>59)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(':',start);
+  if(end<len)
+  {
+    sc->End_hour=value.substring(start,end).toInt();
+    if(sc->End_hour>24)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(',',start);
+  if(end<len)
+  {
+    sc->End_minute=value.substring(start,end).toInt();
+    if(sc->End_minute>59)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(':',start);
+  if(end<len)
+  {
+    sc2.Start_hour=value.substring(start,end).toInt();
+    if(sc2.Start_hour>24)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(',',start);
+  if(end<len)
+  {
+    sc2.Start_minute=value.substring(start,end).toInt();
+    if(sc2.Start_minute>59)return false;
+  }else return false;
+  start=end+1;
+  end=value.indexOf(':',start);
+  if(end<len)
+  {
+    sc2.End_hour=value.substring(start,end).toInt();
+    if(sc2.End_hour>24)return false;
+  }else return false;
+  start=end+1;
+  sc2.End_minute=value.substring(start,len).toInt();
+  if(sc2.End_minute>59) return false;
+  ESP32_Flash.storeSchedule(month,day,sc2);
+  return true;
+}
+
+static bool check_month(String val,int * index)
+{
+  if( val=="Jan" )*index=0;
+  else if( val=="Feb" )*index=1;
+  else if( val=="Mar" )*index=2;
+  else if( val=="Apr" )*index=3;
+  else if( val=="May" )*index=4;
+  else if( val=="Jun" )*index=5;
+  else if( val=="Jul" )*index=6;
+  else if( val=="Aug" )*index=7;
+  else if( val=="Sep" )*index=8;
+  else if( val=="Oct" )*index=9;
+  else if( val=="Nov" )*index=10;
+  else if( val=="Dec" )*index=11;
+  else return false;
+  return true;
 }
 
 static void check_serial_request(void)
@@ -384,13 +703,12 @@ static void check_serial_request(void)
       else
       {
         Serial2.printf("%s",uart.buffer);
-        delay(200);
+        delay(300);
         if(check_serial2())Serial.println(uart2.buffer);
       }
       
     }
   }
-  //if(Serial2.available())Serial.write(Serial2.read());
 }
 
 static void setup_network(void)
@@ -485,15 +803,31 @@ static bool check_serial2()
   return false;
 }
 
-void P_Memory::storeSchedule(uint8_t month, uint8_t day,uint8_t Start_hour, uint8_t Start_minute, uint8_t End_hour, uint8_t End_minute)
+void P_Memory::storeSchedule(uint8_t month, uint8_t day, schedule sc)
 {
   if(month>12 || month==0)return;
   if(day>Number_Of_Days_In_Months[month-1] || day==0)return;    
   int EEPROM_Address=SCHEDULE_START_ADDRESS+month*70+(day-1)*2;
-  uint8_t lsb=(Start_hour<<2) | (Start_minute/15);
-  uint8_t msb=(End_hour<<2) | (End_minute/15);
+  uint8_t lsb=(sc.Start_hour<<2) | (sc.Start_minute/15);
+  uint8_t msb=(sc.End_hour<<2) | (sc.End_minute/15);
   EEPROM.write(EEPROM_Address, lsb);
   EEPROM.write(EEPROM_Address+1, msb);  
+  EEPROM.commit();
+}
+
+schedule P_Memory::getSchedule(uint8_t month, uint8_t day)
+{
+  schedule sc{ .Start_hour=0, .Start_minute=0,.End_hour=0,.End_minute=0};
+  if(month>12 || month==0)return sc;
+  if(day>Number_Of_Days_In_Months[month-1] || day==0)return sc;    
+  int EEPROM_Address=SCHEDULE_START_ADDRESS+month*70+(day-1)*2;
+  uint8_t lsb=EEPROM.read(EEPROM_Address);
+  uint8_t msb=EEPROM.read(EEPROM_Address+1);
+  sc.Start_hour=lsb>>2;
+  sc.Start_minute=(lsb & 0x03)*15;   
+  sc.End_hour=msb>>2;
+  sc.End_minute=(msb & 0x03)*15;
+  return sc;
 }
 
 void P_Memory::clear_all_memory(void)
